7236ddd502f9f8e68b456b2b1a947bf3d7b9d1a1
Henrique dos Santos <henriquesqs@hotmail.com>
2020-04-08 17:38:36 -0300

    Some updates

--- Scientific Initiation/Classification Algorithms/Decision Tree/decisionTree.py
+++ Scientific Initiation/Classification Algorithms/Decision Tree/decisionTree.py
@@ -1,13 +1,13 @@
 import preprocessing as pp
-from sklearn.neural_network import MLPClassifier
+from sklearn import tree
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
-# Creating and trainning our model
-mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=1000)
-mlp.fit(pp.X_train, pp.y_train)
+clf = tree.DecisionTreeClassifier()
+#clf.fit(pp.X_train,pp.y_train)
 
-# Predict
-predicted = mlp.predict(pp.X_test)
+predicted = clf.predict(pp.X_test)
+
+tree.plot_tree(clf.fit(pp.X_train,pp.y_train))
 
 print(accuracy_score(pp.y_test, predicted))
 print(confusion_matrix(pp.y_test,predicted))
--- Scientific Initiation/Classification Algorithms/SVM/svm.py
+++ Scientific Initiation/Classification Algorithms/SVM/svm.py
@@ -1,15 +1,15 @@
-from preprocessing import *
+import preprocessing as pp
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
 # Creating and trainning our model
 svc = SVC(kernel='linear')
-svc.fit(X_train, y_train)
+svc.fit(pp.X_train, pp.y_train)
 
 # Predict
-predicted = svc.predict(X_test)
+predicted = svc.predict(pp.X_test)
 
-print(accuracy_score(y_test, predicted))
-print(confusion_matrix(y_test,predicted))
-print(classification_report(y_test,predicted))
+print(accuracy_score(pp.y_test, predicted))
+print(confusion_matrix(pp.y_test,predicted))
+print(classification_report(pp.y_test,predicted))
 
