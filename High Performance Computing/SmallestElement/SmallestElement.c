3449a1f8813ccff06b62a00b8dd1e28e8e839520
henriquesqs <henriquesqs@hotmail.com>
2020-10-25 12:24:35 -0300

    source code updated

--- High Performance Computing/SmallestElement/SmallestElement.c
+++ High Performance Computing/SmallestElement/SmallestElement.c
@@ -1,71 +1,48 @@
-// Gustavo T. Mastrobuono NUSP 10734411, Henrique de S. Q. dos Santos NUSP 10819029, 
-// Jhordan P. V. Pesantes NUSP 11733353, Witor M. A. de Oliveira NUSP 10692190 e Yorvin A. R. Carrion NUSP 11733332
+// Membros em ordem alfabética:
+// Gustavo T. Mastrobuono NUSP 10734411, 
+// Henrique de S. Q. dos Santos NUSP 10819029, 
+// Jhordan P. V. Pesantes NUSP 11733353, 
+// Witor M. A. de Oliveira NUSP 10692190 e
+// Yorvin A. R. Carrion NUSP 11733332
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <omp.h>
-#include <string.h>
 
 #define T 4 // numero de threads
 
 int main(){
     
-    omp_lock_t lock;
-    omp_init_lock(&lock); // inicializando nosso lock
-    
-    int i = 0, n = 0, pos = 0, comparador = 0, menorLocal, iteracoes = 0, menorGlobal = 0, resto = 0;
+    int i = 0, n = 0, pos = 0;
 
-    scanf("%d", &n); // tamanho do vetor
-    scanf("%d", &pos); // posição do vetor
+    // Esse scanf assume que os valores estão dispostos linha por linha
+    // como no arquivo input.txt deixado como exemplo
+    scanf("%d\n%d", &n, &pos); // le tamanho do vetor e posição de comparação
 
-    int vet[n], vetor_resto[T] = {0};
+    int vet[n], menorLocal = 99999, menorGlobal = 99999;
 
-    for(int i = 0; i < n; i++){
-        scanf("%d", &vet[i]); // lendo valores do vetor
-    }    	
+    for(int i = 0; i < n; i++)
+        scanf("%d", &vet[i]); // le e armazena valores no vetor
 
-    comparador = vet[pos]; // salvando o valor a ser comparado
-    iteracoes = n / T; // limite de iteracoes de cada tarefa
-    resto = n%T; // verificando se é possivel efetuar uma divisao exata dos dados
-
-    for(i = 0; i < resto; i++) // inicializando o vetor de resto 
-		vetor_resto[i] = 1; // ele armazena se a thread precisa fazer mais iterações
-
-    menorGlobal = 99999;
-
-    #pragma omp parallel num_threads(T) private(i) shared(menorGlobal)
-    {   
-
-        // inicializando variaveis para cada processo
-        int menorLocal = 99999;
-        
-        // Esse laço efetua a divisão exata de dados entre os processos disponíveis
-        for (i = omp_get_thread_num() * iteracoes; i < iteracoes + omp_get_thread_num() * iteracoes; i++) {	
-            
-            if((vet[i] > comparador) && (vet[i] < menorLocal)){
+    // Inicializa um loop em paralelo atribuindo n/T iteracoes pra cada 
+    // thread, inicializa menorLocal = 99999 em cada thread localmente
+    // e compartilha a variavel menorGlobal entre os processos
+    #pragma omp parallel for firstprivate(menorLocal)\
+    shared(menorGlobal) schedule(dynamic, n/T)
+        for(i = 0; i < n; i++){
+            if((vet[i] > vet[pos]) && (vet[i] < menorLocal))
                 menorLocal = vet[i];
-            }
-        }
 
-        // verifica se a thread precisa fazer operação a mais para suprir o resto do vetor
-        if(vetor_resto[omp_get_thread_num()] == 1){ 
-            if((vet[iteracoes*T + omp_get_thread_num()] > comparador) && (vet[iteracoes*T + omp_get_thread_num()] < menorLocal)){
-                menorLocal = vet[iteracoes*T + omp_get_thread_num()];
-            }
+            // Após a computação do menor local no bloco recebido pelo
+            // processo, efetua a comparação com o menor global
+            #pragma omp critical
+                if(menorLocal < menorGlobal)
+                    menorGlobal = menorLocal;
         }
-
-        // Após a computação do menor local no bloco recebido pelo processo,
-        // efetua a comparação com o menor global
-        omp_set_lock(&lock);
-        if(menorLocal < menorGlobal){
-            menorGlobal = menorLocal;
-        }
-        omp_unset_lock(&lock);
-    }
     
     if(menorGlobal == 99999)
         printf("-1");
-    else printf("%d\n", menorGlobal);
+    else 
+        printf("%d\n", menorGlobal);
     
     return 0;
     
